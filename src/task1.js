2eda6d078341cd4e4797c314ac0d1e68223debfd
Sergei <se-panfilov@ya.ru>
2017-10-05 22:13:52 +0800

    task 1

---
+++ src/task1.js
@@ -0,0 +1,28 @@
+//Task1
+const products = [
+    {id: 1, price: 10}, {id: 2, price: 11}, {id: 3, price: 1},
+    {id: 4, price: 3}, {id: 5, price: 1}, {id: 6, price: 8},
+    {id: 7, price: 3}, {id: 8, price: 0}, {id: 9, price: 4},
+    {id: 10, price: 5}, {id: 11, price: 9}, {id: 12, price: 13},
+]
+
+/**
+ * @params [Array] products - list of products
+ * @params [Number] options.size - Optional parameter. By default it should be 5
+ **/
+function sortProducts (products = [], options = {}) {
+    const size = (options.size || options.size === 0) ? +options.size : 5
+    const sortedProducets = products.sort((a, b) => {
+        return a.price - b.price
+    })
+
+    return {
+        highest: sortedProducets.slice(0, size),
+        lowest: sortedProducets.slice(sortedProducets.length - size)
+    }
+
+}
+
+const result = sortProducts(products) // {highest: [...], lowest: [...]}
+
+console.info(result)
